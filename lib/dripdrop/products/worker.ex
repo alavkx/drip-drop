f8c6632397aa856eb5fe1e3359f25b627bda4205
Alec Lavoie <alavoie3@gmail.com>
2020-02-17 11:51:24 -0500

    wip

--- lib/dripdrop/products/worker.ex
+++ lib/dripdrop/products/worker.ex
@@ -1,4 +1,5 @@
 defmodule Dripdrop.CrawlSite do
+  use GenServer
   use Task
   use DripdropWeb, :controller
   import Ecto.Query
@@ -7,7 +8,25 @@ defmodule Dripdrop.CrawlSite do
   alias Dripdrop.Product
   alias Dripdrop.SKU
 
-  def start_link(_arg), do: Task.start_link(&poll/0)
+  @name DW
+  
+  # Client API
+  def start_link(opts \\ []) do
+    GenServer.start_link(__MODULE__, :ok, opts ++ [name: DW])
+  end
+
+  # Server Callbacks
+  def init() do
+  
+  end
+
+  def handle_call() do
+    
+  end
+
+  def handle_cast() do
+    
+  end
 
   def poll() do
     main()
@@ -19,15 +38,18 @@ defmodule Dripdrop.CrawlSite do
     end
   end
 
-  def main() do
+  def main() do  
     baseUrl = "https://acrnm.com"
     IO.puts("#{DateTime.utc_now()} - Fetching html from #{baseUrl}")
-
+    # 1st process
     {:ok, %{body: body}} = Mojito.get(baseUrl)
-
     [product_release_msg, sku_restock_msg] =
       body
-      |> parse_product_links
+      |> Floki.parse()
+      |> Floki.find(".tile-list a")
+      |> Floki.attribute("href")
+      |> Enum.filter(&String.contains?(&1, "products"))
+      # N processes
       |> Task.async_stream(Dripdrop.CrawlSite, :crawl_product, [baseUrl],
         max_concurrency: 10,
         ordered: false
@@ -36,38 +58,25 @@ defmodule Dripdrop.CrawlSite do
       |> transpose
       |> build_product_releases_msg
       |> build_sku_restocks_msg
-
     unless is_nil(product_release_msg) do
       post_discord_message(product_release_msg)
     end
-
     unless is_nil(sku_restock_msg) do
       post_discord_message(sku_restock_msg)
     end
-
     IO.puts("#{DateTime.utc_now()} - Finished crawling #{baseUrl}")
-  end
-
-  defp parse_product_links(html) do
-    html
-    |> Floki.parse()
-    |> Floki.find(".tile-list a")
-    |> Floki.attribute("href")
-    |> Enum.filter(&String.contains?(&1, "products"))
   end
 
   def crawl_product(path, base) do
     IO.puts("Crawling page: #{path}")
-
     case Mojito.get(base <> path) do
       {:ok, %{body: body}} ->
         {body, path}
         |> parse_product_info
-        |> insert_or_get_product
-        |> insert_or_get_skus
+        |> get_or_insert_product
+        |> get_or_insert_skus
         |> update_missing_skus_not_in_stock
         |> build_stock_update_msg
-
       {:error, _reason} ->
         {nil, nil}
     end
@@ -78,12 +87,10 @@ defmodule Dripdrop.CrawlSite do
       path
       |> String.split(["products/", "_"])
       |> Enum.drop(1)
-
     skus =
       body
       |> Floki.find("#variety_id option")
       |> Enum.map(fn x -> Floki.text(x) |> String.split(" / ") end)
-
     [description, type, generation, style, price] =
       body
       |> Floki.find(".product-details")
@@ -93,7 +100,6 @@ defmodule Dripdrop.CrawlSite do
       |> String.split(["Type", "Style", "Price", "Gen.", "\n"])
       |> Enum.map(&String.trim/1)
       |> Enum.filter(fn x -> String.length(x) > 0 end)
-
     {skus,
      %{
        model_code: model_code,
@@ -106,9 +112,8 @@ defmodule Dripdrop.CrawlSite do
      }}
   end
 
-  defp insert_or_get_product({skus, product_params}) do
+  defp get_or_insert_product({skus, product_params}) do
     changeset = Product.changeset(%Product{}, product_params)
-
     case Repo.get_by(Product,
            model_code: product_params.model_code,
            season: product_params.season,
@@ -122,42 +127,33 @@ defmodule Dripdrop.CrawlSite do
   defp insert_or_get_sku([color, size], product) do
     case Repo.get_by(SKU, color: color, size: size, product_id: product.id) do
       nil ->
-        insertResponse =
-          product
+        {:new_sku, product
           |> Ecto.build_assoc(:skus)
           |> SKU.changeset(%{color: color, size: size})
-          |> Repo.insert()
-
-        {:new_sku, insertResponse}
-
+          |> Repo.insert()}
       %{in_stock: false} = sku ->
         updateResponse =
           sku
           |> SKU.changeset(%{in_stock: true})
           |> Repo.update()
-
         {:restocked_sku, updateResponse}
-
       sku ->
         {:existing_sku, {:ok, sku}}
     end
   end
 
-  defp insert_or_get_skus({product_type, {:ok, product}, skus}) do
+  defp get_or_insert_skus({product_type, {:ok, product}, skus}) do
     skus = Enum.map(skus, &insert_or_get_sku(&1, product))
-
     {{product_type, product}, skus}
   end
 
   defp update_missing_skus_not_in_stock({{product_type, product}, skus}) do
     sku_ids = Enum.map(skus, fn {_, {_, x}} -> x.id end)
-
     from(s in SKU,
       where: s.product_id == ^product.id and not (s.id in ^sku_ids) and s.in_stock == true,
       select: s
     )
     |> Repo.update_all(set: [in_stock: false])
-
     {{product_type, product}, skus}
   end
 
@@ -168,30 +164,23 @@ defmodule Dripdrop.CrawlSite do
       Enum.filter(skus, fn {sku_type, {_, _}} ->
         sku_type == :restocked_sku || (product_type == :existing_product && sku_type == :new_sku)
       end)
-
     product_url = "https://acrnm.com/products/#{product.model_code}_#{product.season}"
-
     product_link = "[#{product.model_code}](#{product_url})"
-
     product_msg =
       if product_type == :new_product do
         product_link
       end
-
     restocked_skus_msg =
       if length(restocked_skus) > 0 do
         msg = restocked_skus
-        |> Enum.map(fn {_, {_, s}} -> fmt_sku_display_name(s) end)
+        |> Enum.map(fn {_, {_, sku}} -> fmt_sku_display_name(sku) end)
         |> Enum.join(", ")
-
         "#{product_link}: " <> msg
       end
-
     IO.puts("\t#{product.model_code}")
     Enum.each(skus, fn {sku_type, {_, sku}} ->
       IO.puts("\t\t#{sku_type} -> #{fmt_sku_display_name(sku)} / id#{sku.id}")
     end)
-
     {product_msg, restocked_skus_msg}
   end
 
@@ -205,31 +194,25 @@ defmodule Dripdrop.CrawlSite do
     product_msgs = Enum.reject(product_msgs, &is_nil/1)
     msg = "Product drop detected\n" <> Enum.join(product_msgs, " / ")
     release_msg = if length(product_msgs) > 0 do msg end
-
     [release_msg, sku_msgs]
   end
 
   defp build_sku_restocks_msg([release_msg, sku_msgs]) do
     sku_msgs = Enum.reject(sku_msgs, &is_nil/1)
-
     restock_msg =
       if length(sku_msgs) > 0 do
         "Restock detected\n" <> Enum.join(sku_msgs, "\n")
       end
-
     [release_msg, restock_msg]
   end
 
   defp post_discord_message(msg) do
     base = "https://discordapp.com/api"
-
     subneet_prod_hook =
       "/webhooks/597072817544101892/W7edvrXrrYcYgPakgGmgAdkYJLUYOo_vAhTuadlDz0o4lBJ8KdaILAbc0SEiDUkB61_j"
     f1_dev_hook = 
       "/webhooks/596870533354618880/1nsxCaDC-d9D1w_76aEWbZIACgj-n-B-N_iDbXqzLK5MWuiumJ4-IHNeD0BbsaIIe3wL"
-
     hook = if Mix.env == :prod, do: subneet_prod_hook, else: f1_dev_hook
-
     Mojito.post(
       base <> hook,
       [{"content-type", "application/json"}],
