f4377ef9f9807d37c52aead05028751692b94e22
Alec Lavoie <alavoie3@gmail.com>
2020-03-01 13:56:25 -0500

    cleanup css, init tailwind

---
+++ assets/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  theme: {
+    fontFamily: {
+      display: ["Roboto Mono", "monospace"],
+      body: ["Roboto Mono", "monospace"]
+    },
+    extend: {
+      colors: {
+        volt: "rgb(222, 255, 25)",
+        obsidian: "#24292e"
+      }
+    }
+  },
+  variants: {},
+  plugins: []
+};
