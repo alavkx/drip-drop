70ec23494e9c08be7ac4b4ddc11d0df46d75ca55
Alec <alavkx@users.noreply.github.com>
2020-03-02 00:48:18 -0500

    build(env): webhook; secrets (#9)

--- config/prod.exs
+++ config/prod.exs
@@ -5,13 +5,13 @@ config :logger, level: :info
 config :dripdrop, DripdropWeb.Endpoint,
   # Possibly not needed, but doesn't hurt
   http: [port: {:system, "PORT"}],
-  url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 80],
-  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"),
+  url: [host: "${APP_NAME}.gigalixirapp.com", port: 80],
+  secret_key_base: "${SECRET_KEY_BASE}",
   server: true
 
 config :dripdrop, Dripdrop.Repo,
   adapter: Ecto.Adapters.Postgres,
-  url: System.get_env("DATABASE_URL"),
+  url: "${DATABASE_URL}",
   ssl: true,
   # Free tier db only allows 4 connections. Rolling deploys need pool_size*(n+1) connections.
   pool_size: 2
