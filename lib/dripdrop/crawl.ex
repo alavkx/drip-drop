19fa316df3d1bc5f812644a62332e2557aa77165
Alec Lavoie <alavoie3@gmail.com>
2020-03-01 23:46:23 -0500

    refactor(post_message): to handle_cast

--- lib/dripdrop/crawl.ex
+++ lib/dripdrop/crawl.ex
@@ -5,32 +5,58 @@ defmodule Dripdrop.Crawl do
   alias Dripdrop.Repo
   alias Dripdrop.Product
   alias Dripdrop.SKU
-  
+
   def start_link(_) do
     GenServer.start_link(__MODULE__, %{})
   end
-  
+
   @impl true
   def init(state) do
+    run()
     schedule_work()
     {:ok, state}
   end
 
   @impl true
   def handle_info(:work, state) do
-    main()
+    run()
     schedule_work()
     {:noreply, state}
+  end
+
+  @impl true
+  def handle_cast({:post_message, msg}, state) do
+    path =
+      if Mix.env() == :prod do
+        "/webhooks/596870533354618880/1nsxCaDC-d9D1w_76aEWbZIACgj-n-B-N_iDbXqzLK5MWuiumJ4-IHNeD0BbsaIIe3wL"
+      else
+        "/webhooks/596870533354618880/1nsxCaDC-d9D1w_76aEWbZIACgj-n-B-N_iDbXqzLK5MWuiumJ4-IHNeD0BbsaIIe3wL"
+      end
+
+    Mojito.post(
+      "https://discordapp.com/api" <> path,
+      [{"content-type", "application/json"}],
+      Jason.encode!(%{"content" => msg})
+    )
+
+    {:noreply, state}
+  end
+
+  defp post_message(nil), do: nil
+
+  defp post_message(msg) do
+    GenServer.call(__MODULE__, {:post_message, msg})
   end
 
   defp schedule_work do
     Process.send_after(self(), :work, 120_000)
   end
 
-  def main() do
+  def run() do
     baseUrl = "https://acrnm.com"
     IO.puts("#{DateTime.utc_now()} - Fetching html from #{baseUrl}")
     {:ok, %{body: body}} = Mojito.get(baseUrl)
+
     [product_release_msg, sku_restock_msg] =
       body
       |> Floki.parse()
@@ -41,16 +67,15 @@ defmodule Dripdrop.Crawl do
         max_concurrency: 10,
         ordered: false
       )
+      |> Enum.filter(&match?({:ok, _}, &1))
       |> Enum.map(fn {:ok, val} -> val end)
       |> transpose
       |> build_product_releases_msg
       |> build_sku_restocks_msg
-    unless is_nil(product_release_msg) do
-      post_discord_message(product_release_msg)
-    end
-    unless is_nil(sku_restock_msg) do
-      post_discord_message(sku_restock_msg)
-    end
+
+    post_message(product_release_msg)
+    post_message(sku_restock_msg)
+
     IO.puts("#{DateTime.utc_now()} - Finished crawling #{baseUrl}")
   end
 
@@ -63,6 +88,7 @@ defmodule Dripdrop.Crawl do
         |> get_or_insert_skus
         |> update_missing_skus_not_in_stock
         |> build_stock_update_msg
+
       {:error, _reason} ->
         {nil, nil}
     end
@@ -73,10 +99,12 @@ defmodule Dripdrop.Crawl do
       path
       |> String.split(["products/", "_"])
       |> Enum.drop(1)
+
     skus =
       body
       |> Floki.find("#variety_id option")
       |> Enum.map(fn x -> Floki.text(x) |> String.split(" / ") end)
+
     [description, type, generation, style, price] =
       body
       |> Floki.find(".product-details")
@@ -86,6 +114,7 @@ defmodule Dripdrop.Crawl do
       |> String.split(["Type", "Style", "Price", "Gen.", "\n"])
       |> Enum.map(&String.trim/1)
       |> Enum.filter(fn x -> String.length(x) > 0 end)
+
     {skus,
      %{
        model_code: model_code,
@@ -100,6 +129,7 @@ defmodule Dripdrop.Crawl do
 
   defp get_or_insert_product({skus, product_params}) do
     changeset = Product.changeset(%Product{}, product_params)
+
     case Repo.get_by(Product,
            model_code: product_params.model_code,
            season: product_params.season,
@@ -113,16 +143,20 @@ defmodule Dripdrop.Crawl do
   defp insert_or_get_sku([color, size], product) do
     case Repo.get_by(SKU, color: color, size: size, product_id: product.id) do
       nil ->
-        {:new_sku, product
-          |> Ecto.build_assoc(:skus)
-          |> SKU.changeset(%{color: color, size: size})
-          |> Repo.insert()}
+        {:new_sku,
+         product
+         |> Ecto.build_assoc(:skus)
+         |> SKU.changeset(%{color: color, size: size})
+         |> Repo.insert()}
+
       %{in_stock: false} = sku ->
         updateResponse =
           sku
           |> SKU.changeset(%{in_stock: true})
           |> Repo.update()
+
         {:restocked_sku, updateResponse}
+
       sku ->
         {:existing_sku, {:ok, sku}}
     end
@@ -135,38 +169,50 @@ defmodule Dripdrop.Crawl do
 
   defp update_missing_skus_not_in_stock({{product_type, product}, skus}) do
     sku_ids = Enum.map(skus, fn {_, {_, x}} -> x.id end)
+
     from(s in SKU,
       where: s.product_id == ^product.id and not (s.id in ^sku_ids) and s.in_stock == true,
       select: s
     )
     |> Repo.update_all(set: [in_stock: false])
+
     {{product_type, product}, skus}
   end
 
-  defp fmt_sku_display_name(s) do "#{s.size} / #{s.color}" end
+  defp fmt_sku_display_name(s) do
+    "#{s.size} / #{s.color}"
+  end
 
   defp build_stock_update_msg({{product_type, product}, skus}) do
     restocked_skus =
       Enum.filter(skus, fn {sku_type, {_, _}} ->
         sku_type == :restocked_sku || (product_type == :existing_product && sku_type == :new_sku)
       end)
+
     product_url = "https://acrnm.com/products/#{product.model_code}_#{product.season}"
     product_link = "[#{product.model_code}](#{product_url})"
+
     product_msg =
       if product_type == :new_product do
         product_link
       end
+
     restocked_skus_msg =
       if length(restocked_skus) > 0 do
-        msg = restocked_skus
-        |> Enum.map(fn {_, {_, sku}} -> fmt_sku_display_name(sku) end)
-        |> Enum.join(", ")
+        msg =
+          restocked_skus
+          |> Enum.map(fn {_, {_, sku}} -> fmt_sku_display_name(sku) end)
+          |> Enum.join(", ")
+
         "#{product_link}: " <> msg
       end
+
     IO.puts("\t#{product.model_code}")
+
     Enum.each(skus, fn {sku_type, {_, sku}} ->
       IO.puts("\t\t#{sku_type} -> #{fmt_sku_display_name(sku)} / id#{sku.id}")
     end)
+
     {product_msg, restocked_skus_msg}
   end
 
@@ -179,29 +225,23 @@ defmodule Dripdrop.Crawl do
   defp build_product_releases_msg([product_msgs, sku_msgs]) do
     product_msgs = Enum.reject(product_msgs, &is_nil/1)
     msg = "Drop detected\n" <> Enum.join(product_msgs, " / ")
-    release_msg = if length(product_msgs) > 0 do msg end
+
+    release_msg =
+      if length(product_msgs) > 0 do
+        msg
+      end
+
     [release_msg, sku_msgs]
   end
 
   defp build_sku_restocks_msg([release_msg, sku_msgs]) do
     sku_msgs = Enum.reject(sku_msgs, &is_nil/1)
+
     restock_msg =
       if length(sku_msgs) > 0 do
         "Restock detected\n" <> Enum.join(sku_msgs, "\n")
       end
-    [release_msg, restock_msg]
-  end
 
-  defp post_discord_message(msg) do
-    path = if Mix.env == :prod do
-        "/webhooks/596870533354618880/1nsxCaDC-d9D1w_76aEWbZIACgj-n-B-N_iDbXqzLK5MWuiumJ4-IHNeD0BbsaIIe3wL"
-      else
-        "/webhooks/596870533354618880/1nsxCaDC-d9D1w_76aEWbZIACgj-n-B-N_iDbXqzLK5MWuiumJ4-IHNeD0BbsaIIe3wL"
-    end
-    Mojito.post(
-      "https://discordapp.com/api" <> path,
-      [{"content-type", "application/json"}],
-      Jason.encode!(%{"content" => msg})
-    )
+    [release_msg, restock_msg]
   end
 end
